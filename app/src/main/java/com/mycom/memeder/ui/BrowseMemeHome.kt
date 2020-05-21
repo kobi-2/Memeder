b0e7bc317040cc2a20aead3b9acf7deba9f96fe6
kobi-2 <mueeze.nhpg@gmail.com>
2020-05-22 03:59:37 +0600

    browse meme activity setup

---
+++ app/src/main/java/com/mycom/memeder/ui/BrowseMemeHome.kt
@@ -0,0 +1,13 @@
+package com.mycom.memeder.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mycom.memeder.R
+
+class BrowseMemeHome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse_meme_home)
+    }
+}
--- app/src/main/java/com/mycom/memeder/ui/MainActivity.kt
+++ app/src/main/java/com/mycom/memeder/ui/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.mycom.memeder.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.mycom.memeder.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.browse_meme_button)
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v!!.id){
+            R.id.browse_meme_button -> startActivity(Intent(this, BrowseMemeHome::class.java))
+            R.id.upload_meme_button -> Toast.makeText(this, "Upload A Meme!", Toast.LENGTH_SHORT).show()
+            R.id.join_meme_gang_button -> Toast.makeText(this, "Join A Meme Gang!", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
